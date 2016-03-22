e4bd7c29b9d54f883a6f7f8b35c8067e719ec063
Maximilian Schmitt <max@wunderflats.com>
2016-03-22 15:23:09 +0100

    Add option to filter values that should be skipped

--- test/test.js
+++ test/test.js
@@ -284,6 +284,56 @@ suite('Unflatten', function() {
     })
   })
 
+  suite('.filter', function() {
+    var everything = function() { return false }
+    var nothing = function() { return true }
+    var ifHasName = function(obj) { return !obj.name }
+
+    test('Should let a custom check decide if object should be flattened', function () {
+      var fixture = {
+        hello: {
+          world: 'hello',
+          good: {
+            bye: 'my friend'
+          },
+          something: {
+            nested: {
+              pretty: {
+                deep: true
+              },
+              name: 'hello'
+            }
+          },
+          array: ['of', 'values']
+        }
+      }
+
+      assert.deepEqual(flat(fixture, { filter: everything }), fixture)
+
+      assert.deepEqual(flat(fixture, { filter: nothing }), {
+        'hello.world': 'hello',
+        'hello.good.bye': 'my friend',
+        'hello.something.nested.pretty.deep': true,
+        'hello.something.nested.name': 'hello',
+        'hello.array.0': 'of',
+        'hello.array.1': 'values'
+      })
+
+      assert.deepEqual(flat(fixture, { filter: ifHasName }), {
+        'hello.world': 'hello',
+        'hello.good.bye': 'my friend',
+        'hello.something.nested': {
+          pretty: {
+            deep: true
+          },
+          name: 'hello'
+        },
+        'hello.array.0': 'of',
+        'hello.array.1': 'values'
+      })
+    })
+  })
+
   suite('.safe', function() {
     test('Should protect arrays when true', function() {
       assert.deepEqual(flatten({
