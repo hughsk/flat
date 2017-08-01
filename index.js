6789f7281252f41a714eb0d6fee31097fcec4f5f
Antony Jones <aj@desirableobjects.co.uk>
2017-08-01 16:05:05 +0100

    minor refactor for clarity now that complexity has increased

--- index.js
+++ index.js
@@ -23,35 +23,39 @@ function flatten (target, opts) {
     return transformed
   }
 
+  function shouldTraverse (value, transformedValue, currentDepth) {
+    var type = Object.prototype.toString.call(value)
+    var isarray = opts.safe && Array.isArray(value)
+    var isbuffer = isBuffer(value)
+    var isobject = (
+      type === '[object Object]' ||
+      type === '[object Array]'
+    )
+
+    return transformedValue === value &&
+      !isarray &&
+      !isbuffer &&
+      isobject &&
+      Object.keys(value).length &&
+      (!opts.maxDepth || currentDepth < maxDepth)
+  }
+
   function step (object, prev, currentDepth) {
     currentDepth = currentDepth || 1
     Object.keys(object).forEach(function (key) {
       var value = object[key]
-      var isarray = opts.safe && Array.isArray(value)
-      var type = Object.prototype.toString.call(value)
-      var isbuffer = isBuffer(value)
-      var isobject = (
-        type === '[object Object]' ||
-        type === '[object Array]'
-      )
 
       var newKey = prev
         ? prev + delimiter + key
         : key
 
-      const newValue = transform(coercion, key, value)
+      const transformedValue = transform(coercion, key, value)
 
-      if (newValue === value) {
-        if (!isarray &&
-          !isbuffer &&
-          isobject &&
-          Object.keys(value).length &&
-            (!opts.maxDepth || currentDepth < maxDepth)) {
-          return step(value, newKey, currentDepth + 1)
-        }
+      if (shouldTraverse(value, transformedValue, currentDepth)) {
+        return step(value, newKey, currentDepth + 1)
       }
 
-      output[newKey] = newValue
+      output[newKey] = transformedValue
     })
   }
 
