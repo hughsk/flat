3fc349e34607b1df5d8bcd78f1bc1995952492eb
Antony Jones <aj@desirableobjects.co.uk>
2017-12-11 20:12:00 +0000

    refresh dependencies

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# flat [![Build Status](https://secure.travis-ci.org/hughsk/flat.png?branch=master)](http://travis-ci.org/hughsk/flat)
+# flatley [![Build Status](https://secure.travis-ci.org/antony/flatley.png?branch=master)](http://travis-ci.org/antony/flatley)
 
 Take a nested Javascript object and flatten it, or unflatten an object with
 delimited keys.
 
+## Credits
+
+Based on 'flatley' by Hugh Kennedy (http://hughskennedy.com)
+
 ## Installation
 
 ``` bash
-$ npm install flat
+$ npm install flatley
 ```
 
 ## Methods
@@ -17,7 +21,7 @@ Flattens the object - it'll return an object one level deep, regardless of how
 nested the original object was:
 
 ``` javascript
-var flatten = require('flat')
+var flatten = require('flatley')
 
 flatten({
     key1: {
@@ -41,7 +45,7 @@ flatten({
 Flattening is reversible too, you can call `flatten.unflatten()` on an object:
 
 ``` javascript
-var unflatten = require('flat').unflatten
+var unflatten = require('flatley').unflatten
 
 unflatten({
     'three.levels.deep': 42,
@@ -72,7 +76,7 @@ When enabled, both `flat` and `unflatten` will preserve arrays and their
 contents. This is disabled by default.
 
 ``` javascript
-var flatten = require('flat')
+var flatten = require('flatley')
 
 flatten({
     this: [
@@ -140,7 +144,7 @@ This only makes sense on ordered arrays, and since we're overwriting data, shoul
 Maximum number of nested objects to flatten.
 
 ``` javascript
-var flatten = require('flat')
+var flatten = require('flatley')
 
 flatten({
     key1: {
@@ -162,6 +166,8 @@ flatten({
 ### coercion
 
 Optionally run a test/set of tests on your incoming key/value(s) and transform the resulting value if it matches.
+
+This is particularly useful in the case of transforming [https://www.npmjs.com/package/mongoose](mongoose) ObjectIds
 
 ```javascript
 var ObjectId = mongoose.Types.ObjectId
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 /* globals suite test */
 
 var assert = require('assert')
-var flat = require('../index')
-var flatten = flat.flatten
-var unflatten = flat.unflatten
+var flatley = require('../index')
+var flatten = flatley.flatten
+var unflatten = flatley.unflatten
 
 var primitives = {
   String: 'good morning',
@@ -307,22 +307,22 @@ suite('Unflatten', function () {
 
   suite('Overwrite + non-object values in key positions', function () {
     test('non-object keys + overwrite should be overwritten', function () {
-      assert.deepEqual(flat.unflatten({ a: null, 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
-      assert.deepEqual(flat.unflatten({ a: 0, 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
-      assert.deepEqual(flat.unflatten({ a: 1, 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
-      assert.deepEqual(flat.unflatten({ a: '', 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
+      assert.deepEqual(unflatten({ a: null, 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
+      assert.deepEqual(unflatten({ a: 0, 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
+      assert.deepEqual(unflatten({ a: 1, 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
+      assert.deepEqual(unflatten({ a: '', 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
     })
 
     test('overwrite value should not affect undefined keys', function () {
-      assert.deepEqual(flat.unflatten({ a: undefined, 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
-      assert.deepEqual(flat.unflatten({ a: undefined, 'a.b': 'c' }, {overwrite: false}), { a: { b: 'c' } })
+      assert.deepEqual(unflatten({ a: undefined, 'a.b': 'c' }, {overwrite: true}), { a: { b: 'c' } })
+      assert.deepEqual(unflatten({ a: undefined, 'a.b': 'c' }, {overwrite: false}), { a: { b: 'c' } })
     })
 
     test('if no overwrite, should ignore nested values under non-object key', function () {
-      assert.deepEqual(flat.unflatten({ a: null, 'a.b': 'c' }), { a: null })
-      assert.deepEqual(flat.unflatten({ a: 0, 'a.b': 'c' }), { a: 0 })
-      assert.deepEqual(flat.unflatten({ a: 1, 'a.b': 'c' }), { a: 1 })
-      assert.deepEqual(flat.unflatten({ a: '', 'a.b': 'c' }), { a: '' })
+      assert.deepEqual(unflatten({ a: null, 'a.b': 'c' }), { a: null })
+      assert.deepEqual(unflatten({ a: 0, 'a.b': 'c' }), { a: 0 })
+      assert.deepEqual(unflatten({ a: 1, 'a.b': 'c' }), { a: 1 })
+      assert.deepEqual(unflatten({ a: '', 'a.b': 'c' }), { a: '' })
     })
   })
 
