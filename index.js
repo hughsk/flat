1dde8fe3a886c92ae37e92b2b7926abca4e0f4b7
Gie Spaepen <gie.spaepan@zna.be>
2018-01-22 15:19:07 +0100

    Fixed some tests

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ module.exports = flatten
 flatten.flatten = flatten
 flatten.unflatten = unflatten
 
-function flatten(target, opts) {
+function flatten (target, opts) {
   opts = opts || {}
 
-  var delimiter = opts.delimiter || '.';
-  var maxDepth = opts.maxDepth;
-  var ignore = opts.ignore || [];
-  var output = {};
+  var delimiter = opts.delimiter || '.'
+  var maxDepth = opts.maxDepth
+  var ignore = opts.ignore || []
+  var output = {}
 
-  function step(object, prev, currentDepth) {
+  function step (object, prev, currentDepth) {
     currentDepth = currentDepth || 1
     Object.keys(object).forEach(function (key) {
       var value = object[key]
@@ -29,9 +29,8 @@ function flatten(target, opts) {
         : key
 
       if (ignoreKey(key)) {
-        output[newKey] = value;
-      }
-      else if (!isarray && !isbuffer && isobject && Object.keys(value).length &&
+        output[newKey] = value
+      } else if (!isarray && !isbuffer && isobject && Object.keys(value).length &&
         (!opts.maxDepth || currentDepth < maxDepth)) {
         return step(value, newKey, currentDepth + 1)
       } else {
@@ -40,8 +39,8 @@ function flatten(target, opts) {
     })
   }
 
-  function ignoreKey(key) {
-    return ignore.filter(function (x) { return x === key; }).length > 0;
+  function ignoreKey (key) {
+    return ignore.filter(function (x) { return x === key }).length > 0
   }
 
   step(target)
@@ -49,7 +48,7 @@ function flatten(target, opts) {
   return output
 }
 
-function unflatten(target, opts) {
+function unflatten (target, opts) {
   opts = opts || {}
 
   var delimiter = opts.delimiter || '.'
@@ -63,7 +62,7 @@ function unflatten(target, opts) {
 
   // safely ensure that the key is
   // an integer.
-  function getkey(key) {
+  function getkey (key) {
     var parsedKey = Number(key)
 
     return (
