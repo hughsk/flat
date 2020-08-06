c93b27fb5cc9b11cfd55d7378e002bddf0eba0de
Tim Oxley <secoif@gmail.com>
2020-08-06 11:39:23 -0400

    Fix prototype pollution.

--- test/test.js
+++ test/test.js
@@ -445,6 +445,20 @@ suite('Unflatten', function() {
       }
     })
   })
+
+  test('should not pollute prototype', function () {
+      unflatten({
+          '__proto__.polluted': true
+      });
+      unflatten({
+          'prefix.__proto__.polluted': true
+      });
+      unflatten({
+          'prefix.0.__proto__.polluted': true
+      });
+
+      assert.notStrictEqual({}.polluted, true);
+  })
 })
 
 suite('Arrays', function() {
